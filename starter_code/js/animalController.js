fcf7a31a65562bddc8f2eb810186e24c7e1f7db2
Adrianna Marini <adrimarini>
2017-10-11 19:02:22 -0700

    adds animal controller

---
+++ starter_code/js/animalController.js
@@ -0,0 +1,43 @@
+angular.module('IntroToAngularApp')
+    .controller('AnimalController', AnimalController);
+
+    function AnimalController() {
+      var self = this
+      self.awesome = true
+      self.reptiles = [
+        {
+          type: "snake",
+          name: "Henry",
+          age: 2
+        },
+        {
+          type: "lizard",
+          name: "Sal",
+          age: 3
+        },
+        {
+          type: "croc",
+          name: "Amy",
+          age: 6
+        }
+      ];
+    self.mamals = [
+      {
+        type: "bunny",
+        name: "Mr. floofer",
+        age: 5
+      },
+      {
+        type: "giraffe",
+        name: "Sheldon",
+        age: 4
+
+      },
+      {
+        type: "elephant",
+        name: "Tess",
+        age: 15
+      }
+    ];
+
+}
