09363fbf56183701949826899c1d732c9875d80b
Adrianna Marini <adrimarini>
2017-10-12 21:27:57 -0700

    adds like and dislike buttons, but they only work for reptiles and only the 1st one..... gettin there

--- starter_code/js/animalController.js
+++ starter_code/js/animalController.js
@@ -10,19 +10,25 @@ angular.module('IntroToAngularApp')
           type: "snake",
           name: "Henry",
           age: 2,
-          photo: 'http://www.newdesignfile.com/postpic/2010/08/green-snake-cartoon_352065.jpg'
+          photo: 'http://www.newdesignfile.com/postpic/2010/08/green-snake-cartoon_352065.jpg',
+          likes: 0,
+          dislikes: 0
         },
         {
           type: "lizard",
           name: "Sal",
           age: 3,
-          photo: 'https://i.pinimg.com/originals/f3/5c/de/f35cdeaf6651c6cb242b2fbc8ac89f0b.jpg'
+          photo: 'https://i.pinimg.com/originals/f3/5c/de/f35cdeaf6651c6cb242b2fbc8ac89f0b.jpg',
+          likes: 0,
+          dislikes: 0
         },
         {
           type: "croc",
           name: "Amy",
           age: 6,
-          photo: 'https://i.pinimg.com/originals/2c/c3/29/2cc329be86b3c56b45f8af4b8ffd8470.jpg'
+          photo: 'https://i.pinimg.com/originals/2c/c3/29/2cc329be86b3c56b45f8af4b8ffd8470.jpg',
+          likes: 0,
+          dislikes: 0
         }
       ];
     self.mamals = [
@@ -30,21 +36,30 @@ angular.module('IntroToAngularApp')
         type: "bunny",
         name: "Mr. floofer",
         age: 5,
-        photo: 'https://i.pinimg.com/originals/bd/2e/e9/bd2ee9e08816a9a0801f056597230fac.png'
+        photo: 'https://i.pinimg.com/originals/bd/2e/e9/bd2ee9e08816a9a0801f056597230fac.png',
+        likes: 0,
+        dislikes: 0
       },
       {
         type: "giraffe",
         name: "Sheldon",
         age: 4,
-        photo: 'https://previews.123rf.com/images/tigatelu/tigatelu1302/tigatelu130200011/18047043-Cute-giraffe-cartoon-Stock-Vector-drawing.jpg'
+        photo: 'https://previews.123rf.com/images/tigatelu/tigatelu1302/tigatelu130200011/18047043-Cute-giraffe-cartoon-Stock-Vector-drawing.jpg',
+        likes: 0,
+        dislikes: 0
 
       },
       {
         type: "elephant",
         name: "Tess",
         age: 15,
-        photo: 'http://elephant-pictures.clipartonline.net/_/rsrc/1379429378550/baby-elephant-dark-blue/Cartoon_Elephant_2.png.1379429378324.png?height=320&width=320'
+        photo: 'http://elephant-pictures.clipartonline.net/_/rsrc/1379429378550/baby-elephant-dark-blue/Cartoon_Elephant_2.png.1379429378324.png?height=320&width=320',
+        likes: 0,
+        dislikes: 0
       }
     ];
 
+    self.plusOne = function(index) {
+      self.reptiles[0].likes++;
+    };
 }
